564861334919329ac5d629465fe42957d8dd18e4
Kinjal kunjadiya <kunjadiyakinjal1407@gmail.com>
2022-01-11 19:30:58 -0500

    Nav

--- lib/nav.dart
+++ lib/nav.dart
@@ -1,11 +1,10 @@
-
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:order_page/cart.dart';
+import 'package:order_page/bag_screen.dart';
 import 'package:order_page/create%20ui.dart';
 import 'package:order_page/premade.dart';
 import 'package:order_page/profile.dart';
 import 'startScreen.dart';
+
 class nav extends StatefulWidget {
   const nav({Key? key}) : super(key: key);
 
@@ -16,54 +15,50 @@ class nav extends StatefulWidget {
 class _navState extends State<nav> {
   int selectedIndex = 0;
   final screens = <Widget>[
-    page1(),
-   premade(),
-    create(),
-    cart(),
-    profile()
+    const page1(),
+    const PreMade(),
+    const create(),
+    const BagScreen(),
+    const Profile()
   ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
+    return SafeArea(
+      child: Scaffold(
         appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0),
-    child: AppBar(
-    leading: Icon(Icons.add_location_outlined),
-    title: Text("MakeMyCake"),
-    centerTitle: true,
-    backgroundColor: Color.fromRGBO(118, 60, 0, 10),
-    )),
-
-        body:screens[selectedIndex] ,
-
-          bottomNavigationBar: Padding(
-            padding: const EdgeInsets.all(0),
-            child: BottomNavigationBar(
-
-              backgroundColor: Color.fromRGBO(118, 60, 0, 10),
+            preferredSize: const Size.fromHeight(60.0),
+            child: AppBar(
+              leading: const Icon(Icons.add_location_outlined),
+              title: const Text("MakeMyCake"),
+              centerTitle: true,
+              backgroundColor: const Color.fromRGBO(118, 60, 0, 10),
+            )),
+        body: screens[selectedIndex],
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(0),
+          child: BottomNavigationBar(
+              backgroundColor: const Color.fromRGBO(118, 60, 0, 10),
               selectedItemColor: Colors.lightBlueAccent,
               unselectedItemColor: Colors.grey,
               showUnselectedLabels: true,
               items: const <BottomNavigationBarItem>[
-
-
-
                 BottomNavigationBarItem(
-
-                  icon: Icon(Icons.home,),
+                  icon: Icon(
+                    Icons.home,
+                  ),
                   label: 'Home',
                 ),
                 BottomNavigationBarItem(
-
                   icon: Icon(Icons.cake),
                   label: 'PreMade',
-
                 ),
                 BottomNavigationBarItem(
-
-                  icon: Icon(Icons.add_circle_rounded,size: 35,color: Colors.orangeAccent,),
+                  icon: Icon(
+                    Icons.add_circle_rounded,
+                    size: 35,
+                    color: Colors.orangeAccent,
+                  ),
                   label: 'Create',
                 ),
                 BottomNavigationBarItem(
@@ -79,10 +74,9 @@ class _navState extends State<nav> {
                 setState(() {
                   selectedIndex = index;
                 });
-              }
-            ),
-          ),
-
-        ));
+              }),
+        ),
+      ),
+    );
   }
 }
